79541fdf3af43a193a8769d0a17e71ad976f963b
GokulRajS1998 <gokul.shibi10@gmail.com>
2021-10-06 18:54:04 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <form>
+        <h1>Registration form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input type="text" name="username" placeholder="username" />
+          </div>
+
+          <div className="field">
+            <label>DOB</label>
+            <input type="date" name="DOB" placeholder="DOB" />
+          </div>
+
+          <div className="field">
+            <label>Class</label>
+            <select id="class" name="class">
+              <option value=""></option>
+              <option value="II">II</option>
+              <option value="III">III</option>
+              <option value="IV">IV</option>
+              <option value="V">V</option>
+              <option value="VI">VI</option>
+              
+            </select>
+          </div>
+
+          <div className="field">
+            <label>Division</label>
+            <input type="dvision" name="division" placeholder="division" />
+          </div>
+
+          <div className="field">
+            <label>Gender</label>
+            <input type="gender" name="gender" placeholder="gender" />
+          </div>
+          <button className="fluid ui button">Submit</button>
+        </div>
+      </form> */}
+      <Form>
+
+      </Form>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,184 @@
+import React, { Component } from "react";    
+import axios from "axios";
+import './Form.css'    
+    
+class Form extends Component {    
+    constructor(props) {    
+        super(props);    
+        this.state = {    
+            studName: '',    
+               
+            dob: '',    
+           standard: 'select',    
+            division: 'select',    
+            gender: '',    
+            formErrors: {}    
+        };    
+    
+        this.initialState = this.state;  
+        this.onValueChange = this.onValueChange.bind(this);  
+    }    
+
+    onValueChange(event) {
+        this.setState({
+          selectedOption: event.target.value
+        });
+      }
+    
+    handleFormValidation() {    
+        const { studName,  dob, standard , division, gender } = this.state;    
+        let formErrors = {};    
+        let formIsValid = true;    
+    
+        //Student name     
+        if (!studName) {    
+            formIsValid = false;    
+            formErrors["studNameErr"] = "Name is required.";    
+        }    
+    
+       
+    
+        //DOB    
+        if (!dob  ) {    
+            formIsValid = false;    
+            formErrors["dobErr"] = "Date of birth is required.";    
+        }    
+        
+    
+        //standard    
+        if (standard === '' || standard === "select") {    
+            formIsValid = false;    
+            formErrors["standardErr"] = "Select class.";    
+        }    
+    
+        //division   
+        if (division === '' || division === "select") {    
+            formIsValid = false;    
+            formErrors["divisionErr"] = "Select division.";    
+        }    
+    
+        console.log(gender)
+        //gender  
+        if (gender === '') {    
+            formIsValid = false;    
+            formErrors["genderErr"] = "Select gender.";    
+        }    
+    
+        this.setState({ formErrors: formErrors });    
+        return formIsValid;    
+    }    
+    
+    handleChange = (e) => {    
+        const { name, value } = e.target;    
+        this.setState({ [name]: value });    
+    }    
+    
+    handleSubmit = (e) => {    
+        e.preventDefault();    
+    
+        if (this.handleFormValidation()) {   
+            axios.post('/user', {
+                name: this.state.studName,
+                class: this.state.standard,
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+               
+            this.setState(this.initialState)    
+        }    
+    }    
+    
+    render() {    
+    
+        const { studNameErr,  dobErr, standardErr, divisionErr, genderErr } = this.state.formErrors;    
+    
+        return (    
+            <div className="formDiv">    
+                <h3 style={{ textAlign: "center" }} >Student Details Form</ h3>    
+                <div>    
+                    <form onSubmit={this.handleSubmit}>    
+                        <div>    
+                            <label htmlFor="studName">Name</label>    
+                            <input type="text" name="studName"    
+                                value={this.state.studName}    
+                                onChange={this.handleChange}    
+                                placeholder="Your name.."    
+                                className={studNameErr ? ' showError' : ''} />    
+                            {studNameErr &&    
+                                <div style={{ color: "red", paddingBottom: 10 }}>{studNameErr}</div>    
+                            }    
+    
+                        </div>    
+                          
+                        <div>    
+                            <label htmlFor="text">Date of Birth </label>    
+                            <input type="date" name="dob"    
+                                value={this.state.dob}    
+                                onChange={this.handleChange}    
+                                placeholder="DD/MM/YYYY.."    
+                                className={dobErr ? ' showError' : ''} />    
+                            {dobErr &&    
+                                <div style={{ color: "red", paddingBottom: 10 }}>{dobErr}</div>    
+                            }    
+                        </div>    
+                        <div>    
+                            <label htmlFor="standard">Class</label>    
+                            <select name="standard" onChange={this.handleChange}    
+                                className={standardErr ? ' showError' : ''}    
+                                value={this.state.standard} >    
+                                <option value="select">--Select--</option>    
+                                <option value="I">I</option>    
+                                <option value="II">II</option>    
+                                <option value="III">III</option>    
+                                <option value="IV">IV</option>    
+                                <option value="V">V</option>    
+                                <option value="VI">VI</option>    
+                                <option value="VII">VII</option>    
+                                <option value="VIII">VIII</option>    
+                                <option value="IX">IX</option>    
+                                <option value="X">X</option>    
+                                <option value="XI">XI</option>    
+                                <option value="XII">XII</option>    
+                            </select>    
+                            {standardErr &&    
+                                <div style={{ color: "red", paddingBottom: 10 }}>{standardErr}</div>    
+                            }    
+                        </div>    
+                        <div>    
+                        <label htmlFor="division">Division</label>    
+                            <select name="division" onChange={this.handleChange}    
+                                className={divisionErr ? ' showError' : ''}    
+                                value={this.state.division} >    
+                                <option value="select">--Select--</option>    
+                                <option value="A">A</option>    
+                                <option value="B">B</option>    
+                                <option value="C">C</option>    
+                                   
+                            </select>      
+                            {divisionErr &&    
+                                <div style={{ color: "red", paddingBottom: 10 }}>{divisionErr}</div>    
+                            }    
+                        </div>    
+                        <div>    
+                            <label htmlFor="gender">Gender</label>    
+                           
+                            <input type="radio" value="Male" name="gender" onChange={this.handleChange} /> Male  
+                            <input type="radio" value="Female" name="gender" onChange={this.handleChange} /> Female 
+                            <input type="radio" value="Other" name="gender"  onChange={this.handleChange} /> Other   
+                            {genderErr &&    
+                                <div style={{ color: "red", paddingBottom: 10 }}>{genderErr}</div>    
+                            }    
+                        </div>    
+                        <input type="submit" value="Submit" />    
+                    </form>    
+                </div>    
+            </div >    
+        )    
+    }    
+}    
+    
+export default Form;
